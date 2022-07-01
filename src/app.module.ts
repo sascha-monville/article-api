4c679077789dc6a3e7f82c95c65f0166b65c7e69
Sascha Monville <sascha.monville.eu>
2022-07-01 11:29:53 +0200

    feat: add boilerplate + dependencies for swagger and mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticlesModule } from './articles/articles.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ArticlesModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'example',
+    database: 'articles',
+    entities: [],
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
